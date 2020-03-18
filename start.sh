4d0f79f28a0556748a1c0cb1c611ccbbeb07ff47
wgnpj2cg <60536607+wgnpj2cg@users.noreply.github.com>
2020-03-18 21:08:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,10 @@ if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	chmod +x on-complete.sh
 fi
 
+tar -zxvf cloudreve_3.0.0-rc1_linux_amd64.tar.gz
+rm -rf cloudreve_3.0.0-rc1_linux_amd64.tar.gz
 chmod +x ./cloudreve
-nohup ./cloudreve > cloudreve.file 2>&1 &   
+nohup ./cloudreve > cloudreve.file 2>&1 &
 echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
 aria2c --conf-path=aria2c.conf&
 yarn start
